26e1c75bae99df4457f7b42e9615cbf71ff6cbcb
Saksham Wadhwa <sakshamwadhwa1000@gmail.com>
2025-09-13 03:08:32 +0530

    make some more func in app.jsx

--- practicereact/src/App.jsx
+++ practicereact/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { Todoprovider } from './contexts'
 
@@ -21,6 +21,21 @@ function App() {
     setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo , completed : !prevTodo.completed} : prevTodo))
   }
 
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos && todos.length > 0) {
+      setTodos(todos)
+    }
+  } , [])
+
+  useEffect((
+    localStorage.setItem("todos",JSON.stringify(todos))
+  ),[todos])
+
+
+  
   return (
     <Todoprovider value={{todos,addTodo,updatedTodo,deleteTodo,toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
